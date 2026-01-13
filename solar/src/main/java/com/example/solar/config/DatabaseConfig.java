9f1bef2fe310d1618fa93014bd400ac420c69469
mayowa-id <idowumayowa02@gmail.com>
2026-01-13 10:01:38 +0100

    implemented api response

--- solar/src/main/java/com/example/solar/config/DatabaseConfig.java
+++ solar/src/main/java/com/example/solar/config/DatabaseConfig.java
@@ -1,4 +1,12 @@
 package com.example.solar.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.solarconnect")
+@EnableTransactionManagement
 public class DatabaseConfig {
+
 }
